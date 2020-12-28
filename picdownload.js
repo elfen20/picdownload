d7655ccffd4c1fac4b9b33af2c9af164a00eafe8
Gernot Lenkner <g.lenkner@outlook.de>
2020-12-28 11:11:03 +0100

    compatibility extended

--- picdownload.js
+++ picdownload.js
@@ -1,19 +1,20 @@
 var pDownload = {
     mouseX: 0,
     mouseY: 0,
+    onKeyDown: function(e) {
+        if (e.key == '<') {
+            var el = document.elementFromPoint(pDownload.mouseX, pDownload.mouseY);
+            if (el) {
+                console.log(el);
+                if (el.tagName == "IMG") console.log("Image!");
+            }
+        }
+    },
+    onMouseMove: function(e) {
+        pDownload.mouseX = e.x;
+        pDownload.mouseY = e.y;
+    }
 };
 
-document.addEventListener('keydown', (event) => {
-    if (event.key == '<') {
-        var el = document.elementFromPoint(pDownload.mouseX, pDownload.mouseY);
-        if (el) {
-            console.log(el);
-            if (el.tagName == "IMG") console.log("Image!");
-        }
-    }
-}, false);
-
-document.addEventListener('mousemove', (event) => {
-    pDownload.mouseX = event.x;
-    pDownload.mouseY = event.y;
-}, false);
+document.addEventListener('keydown', pDownload.onKeyDown, false);
+document.addEventListener('mousemove', pDownload.onMouseMove, false);
