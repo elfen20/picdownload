3e1988d3b10cd700ef4114a4fbe5f2f386840835
Gernot Lenkner <g.lenkner@outlook.de>
2020-12-29 15:30:41 +0100

    added download action

--- background.js
+++ background.js
@@ -4,4 +4,23 @@ function startDownload() {
     console.log("start download");
 }
 
+function notify(message) {
+    var fname = message.img.split('#').shift().split('?').shift().split('/').pop();
+    browser.downloads.download({
+        url : message.img,
+        filename : 'n1/' + fname,
+        conflictAction : 'uniquify'
+    });
+    /*
+    browser.notifications.create({
+        "type": "basic",
+        "iconUrl": browser.extension.getURL("link.png"),
+        "title": "You clicked a link!",
+        "message": message.img
+      });
+      */
+}
+
 browser.browserAction.onClicked.addListener(startDownload);
+browser.runtime.onMessage.addListener(notify);
+
